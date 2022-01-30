070907bc2edde65b05ebf1e90ee3b0dd241c0cfc
razorrazing <7070929+razorrazing@users.noreply.github.com>
2022-01-30 11:48:07 -0500

    Update run.ps1

--- HuduVeeamTimerTrigger/run.ps1
+++ HuduVeeamTimerTrigger/run.ps1
@@ -2,7 +2,7 @@ using namespace System.Net
 
 param($Timer)
 
-$InstanceId = Start-NewOrchestration -FunctionName 'HuduVeeeamOrchestrator'
+$InstanceId = Start-NewOrchestration -FunctionName 'HuduVeeamOrchestrator'
 Write-Host "Started orchestration with ID = '$InstanceId'"
 
 $Response = New-OrchestrationCheckStatusResponse -Request $timer -InstanceId $InstanceId
